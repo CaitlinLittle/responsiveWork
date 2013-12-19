d73b209b9efc0ab30f7a5fae8968a1bc30837ee3
CaitlinLittle <caits941@gmail.com>
2013-12-19 14:33:06 -0800

    Add index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+	body {
+		background-color:#DCDBD9;
+		color:#2C2C2C;
+		font: normal 100% Cambria, Georgia, serif;
+	}
+	
+	#page {
+		width:
+	}
+</style>
+<title>Responsive</title>
+</head>
+<body>
+	<div id="page">
+		<h1>Some words about some things!<a href="#">And a link</a></h1>
+	</div>
+	
+</body>
+</html>
