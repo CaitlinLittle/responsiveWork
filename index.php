a40a92155845ffad6bf7c9f3d45aa16c8048a78d
CaitlinLittle <caits941@gmail.com>
2013-12-19 16:05:13 -0800

    updated index.php

--- index.php
+++ index.php
@@ -12,15 +12,52 @@
 	}
 	
 	#page {
-		width:
+	  margin: 36px auto;
+	  width: 90%;
+	}
+	.blog {
+	  margin: 0 auto 53px;
+	  width: 93.75%; /* 900px / 960px */
+	}
+	.blog .main {
+	  float: left;
+	 width: 62.888889%; /* 566px / 900px */
+	}
+	.blog .other {
+	  float: right;
+	  width: 36.777778%; /* 331px / 900px */
+	}
+
+	h1 {
+		font-size: 1.5em; /* 24/16 px */
+		font-style:italic;
+		font-weight:normal;
+	}
+	
+	h1 a {
+		font: bold 0.458333333333333em Calibri, Optima, »
+	      Arial, sans-serif;  /* 11px / 24px */
+	    color: #747474;
+	    letter-spacing: 0.15em;
+	    text-transform: uppercase;
+	    text-decoration: none;
 	}
 </style>
 <title>Responsive</title>
 </head>
 <body>
 	<div id="page">
-		<h1>Some words about some things!<a href="#">And a link</a></h1>
-	</div>
+			<div class="blog section">
+				<h1 class="lede">Recently in <a href="#">The Bot
+				Blog</a></h1>
+				<div class="main">
+					...
+				</div><!-- /end .main -->
+				<div class="other">
+					...
+				</div><!-- /end .other -->
+			</div><!-- /end .blog.section -->
+		</div><!-- /end #page -->
 	
 </body>
 </html>
